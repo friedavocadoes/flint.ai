4351ad20725d046dd34c481205838368f8e98255
gautham <desertncamel@gmail.com>
2025-05-28 11:09:18 +0530

    navbar final touches

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,67 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { ModeToggle } from "./ui/themeToggle";
+import { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 
-function Navbar() {
+export default function Navbar() {
+  const [active, setActive] = useState<string | null>(null);
   return (
     <>
-      <div className="w-full h-13 bg-background/40 border-b border-foregorund flex items-center px-10 z-100 absolute backdrop-blur-md font-outfit">
+      <div className="fixed w-full h-14 bg-background/40 border-b border-foregorund flex items-center px-4 md:px-10 z-10 backdrop-blur-md font-outfit">
         <div>
-          <Link href="/" className="font-bold">
+          <Link href="/" className="font-bold text-2xl">
             Flint.ai
           </Link>
         </div>
-        <div className="mx-auto text-gray-400 space-x-6">
-          <Link
-            href="#"
-            className="hover:text-gray-200 transition duration-200"
+        <div className="mx-auto">
+          <Menu
+            setActive={setActive}
+            className="flex text-gray-400 space-x-3 md:space-x-6"
           >
-            Prepare
-          </Link>
-          <Link
-            href="#"
-            className="hover:text-gray-200 transition duration-200"
-          >
-            Resume Score
-          </Link>
+            <MenuItem setActive={setActive} active={active} item="Products">
+              <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+                <ProductItem
+                  title="Resume Analyser"
+                  href="#"
+                  src="/thumbs/resume.jpg"
+                  description="Analyze and score your Resume with AI to bust through the ATS'"
+                />
+                <ProductItem
+                  title="Prepare with Flint"
+                  href="#"
+                  src="/thumbs/prep.jpeg"
+                  description="Create a career roadmap and land your dream job."
+                />
+                <ProductItem
+                  title="Mock Interviews"
+                  href="#"
+                  src="/thumbs/interview.jpg"
+                  description="Prepare yourself with sample interviews created with AI"
+                />
+                <ProductItem
+                  title="Discussions"
+                  href="#"
+                  src="/thumbs/discussions.jpg"
+                  description="Engage in curiosity driven discussions."
+                />
+              </div>
+            </MenuItem>
+            <MenuItem setActive={setActive} active={active} item="Support">
+              <div className="flex flex-col space-y-4 text-sm">
+                <ProductItem
+                  title="Go premium"
+                  href="#"
+                  src="/thumbs/pro.jpg"
+                  description="Solo • Team • Business"
+                />
+                <HoveredLink href="#">Contact us</HoveredLink>
+                <HoveredLink href="#">Raise an Issue</HoveredLink>
+                <HoveredLink href="#">Documentation</HoveredLink>
+              </div>
+            </MenuItem>
+          </Menu>
         </div>
         <div>
           <ModeToggle />
@@ -32,5 +70,3 @@ function Navbar() {
     </>
   );
 }
-
-export default Navbar;
