16a8ac1d1f3b714e6459b3e3b5062814b25d46df
gautham <desertncamel@gmail.com>
2026-06-09 11:17:25 +0400

    Update route.js

--- src/app/api/gemini/route.js
+++ src/app/api/gemini/route.js
@@ -14,7 +14,7 @@ export async function POST(req) {
 
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-2.5-flash",
       contents: prompt,
     });
 
